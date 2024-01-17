338746325060de82efd8d12d70429b3564076f0a
Michail Spinelli <michailspinelli96@gmail.com>
2024-01-17 17:23:27 +0100

    fine

---
+++ my-vue-app/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return { AppTitle: 'La mia prima app con Vite!' }
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+  </header>
+  
+</template>
+
+<style scoped>
+</style>
+
